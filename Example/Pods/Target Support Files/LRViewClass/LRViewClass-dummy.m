bb01ddcdbf159ee8252eaf7ddf94ef252bbb4691
18602420243 <18602420243@163.com>
2020-07-03 10:42:11 +0800

    1.0.0

---
+++ Example/Pods/Target Support Files/LRViewClass/LRViewClass-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LRViewClass : NSObject
+@end
+@implementation PodsDummy_LRViewClass
+@end
---
+++ Example/Pods/Target Support Files/LRViewClass/LRViewClass-umbrella.h
@@ -0,0 +1,24 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ControllerPopByInteractivePopGestureRecognizer.h"
+#import "LRDefines.h"
+#import "LRNavigationBar.h"
+#import "LRNavigationController.h"
+#import "LRTabBarViewController.h"
+#import "LRUISize.h"
+#import "LRViewClass.h"
+#import "LRViewController.h"
+
+FOUNDATION_EXPORT double LRViewClassVersionNumber;
+FOUNDATION_EXPORT const unsigned char LRViewClassVersionString[];
+
